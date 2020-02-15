287fcde01e28e016e39a0c75a9337dfc6783132d
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2020-02-15 21:25:49 +0700

    update product-list
    -thêm vào một dropdown để khách hàng chọn loại giá
    -thêm ảnh,mô tả,giá vào cho từng sản phẩm

--- js/myScripts.js
+++ js/myScripts.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
     // executes when HTML-Document is loaded and DOM is ready
     console.log("document is ready");
 
-
     $(".card").hover(
         function() {
-            $(this).addClass('shadow-lg').css('cursor', 'pointer');
+            $(this)
+                .addClass("shadow-lg")
+                .css("cursor", "pointer");
         },
         function() {
-            $(this).removeClass('shadow-lg');
+            $(this).removeClass("shadow-lg");
         }
     );
 
-    // document ready  
+    // document ready
 });
+
+/* Huy */
+
+$("#your-custom-id-button").mdbDropSearch();
+
+/* END Huy */
